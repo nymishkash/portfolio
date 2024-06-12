06314efa3bb75ce2234fd255e5fa6df509831c11
nymishkash <reachnymish@gmail.com>
2024-06-12 13:21:04 +0530

    added projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,34 +1,52 @@
 import React, { Component } from 'react';
 
 export default class Portfolio extends Component {
-  render() {
-    const portfolioItems = [
-      {
-        name: "Python Web Scraper",
-        description: "Web scraper built using BeautifulSoup to retrieve and compare prices from Amazon and Flipkart.",
-        imgurl: "images/portfolio/web-scraper.png",
-        modalId: "https://github.com/nymishkash/python-web-scraper"
-      },
-      {
-        name: "Streambox by Nymish",
-        description: "An audio streaming web application built using HTML, CSS and Javascript.  ",
-        imgurl: "images/portfolio/streambox.jpeg",
-        modalId: "https://github.com/nymishkash/streambox-by-nymish"
-      },
-      {
-        name: "Razorpay Payment Gateway Integration",
-        description: "A payment gateway integration using Razorpay API.",
-        imgurl: "images/portfolio/payment.jpeg",
-        modalId: "modal-03"
-      },
-      {
-        name: "Car Rental System",
-        description: "A car rental system built using SpringBoot, MySQL and Java backend.",
-        imgurl: "images/portfolio/car-rental.jpeg",
-        modalId: "https://github.com/nymishkash/car-rental-system"
-      }
-    ];
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      portfolioItems: [
+        {
+          name: "Python Web Scraper",
+          description: "Web scraper built using BeautifulSoup to retrieve and compare prices from Amazon and Flipkart.",
+          imgurl: "images/portfolio/web-scraper.png",
+          modalId: "https://github.com/nymishkash/python-web-scraper"
+        },
+        {
+          name: "Streambox by Nymish",
+          description: "An audio streaming web application built using HTML, CSS and Javascript.  ",
+          imgurl: "images/portfolio/streambox.jpeg",
+          modalId: "https://github.com/nymishkash/streambox-by-nymish"
+        },
+        {
+          name: "Razorpay Payment Gateway Integration",
+          description: "A payment gateway integration using Razorpay API.",
+          imgurl: "images/portfolio/payment.jpeg",
+          modalId: "https://github.com/nymishkash/payment-service"
+        },
+        {
+          name: "Car Rental System",
+          description: "A car rental system built using SpringBoot, MySQL and Java backend.",
+          imgurl: "images/portfolio/car-rental.jpeg",
+          modalId: "https://github.com/nymishkash/car-rental-system"
+        },
+        {
+          name: "CLI Image Editor",
+          description: "Created a Command Line Based Image Editor using Java.",
+          imgurl: "images/portfolio/image-editor.jpeg",
+          modalId: "https://github.com/nymishkash/cli-image-editor-java"
+        },
+        {
+          name: "Tic Tac Toe",
+          description: "Created game of tic tac toe with computer opponent using Java SpringBoot framework.",
+          imgurl: "images/portfolio/tic-tac-toe.jpeg",
+          modalId: "https://github.com/nymishkash/tic-tac-toe"
+        }
+      ]
+    };
+  }
+  render() {
+    const { portfolioItems } = this.state;
     return (
       <section id="portfolio">
         <div className="row">
@@ -38,7 +56,7 @@ export default class Portfolio extends Component {
               {portfolioItems.map((item, index) => (
                 <div key={index} className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a href={`${item.modalId}`}>
+                    <a href={item.modalId} target="_blank">
                       <img src={item.imgurl} className="item-img" alt={item.name} />
                       <div className="overlay">
                         <div className="portfolio-item-meta">
